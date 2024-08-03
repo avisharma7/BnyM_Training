ed6b4fcc76f2e728d11fdd549307aadb31b86eaf
Aviral Sharma <aviral.np12@gmail.com>
2024-08-03 05:36:48 +0530

    webApp

---
+++ webApp/backened-webApp/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- Corrected INSERT statements
+
+INSERT INTO course (id, author, name)
+VALUES (1, 'AKRITI', 'BIO');
+
+INSERT INTO course (id, author, name)
+VALUES (2, 'AVIRAL', 'OS');
+
+INSERT INTO course (id, author, name)
+VALUES (3, 'AVISAK', 'BIOOS');
